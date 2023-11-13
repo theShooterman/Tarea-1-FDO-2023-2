a83f977c8c32959f04e076da1b15a8c39ca3eca3
Replit user <>
2023-11-13 14:39:43 +0000

    feat: agrega el endpoint create

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 import os
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 VERSION = "v0.1.0"
 
 app = Flask(__name__)
 
+
 def get_db_connection():
-    conn = psycopg2.connect(host=os.environ['HOST'],
-                            database=os.environ['DB'],
-                            user=os.environ['DB'],
-                         password=os.environ['DB_PASSWORD'])
-    return conn
+  conn = psycopg2.connect(host=os.environ['HOST'],
+                          database=os.environ['DB'],
+                          user=os.environ['DB'],
+                          password=os.environ['DB_PASSWORD'])
+  return conn
 
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM books;')
-    books = cur.fetchall()
-    cur.close()
-    conn.close()
-    return render_template('index.html', books=books, version=VERSION)
+  conn = get_db_connection()
+  cur = conn.cursor()
+  cur.execute('SELECT * FROM books;')
+  books = cur.fetchall()
+  cur.close()
+  conn.close()
+  return render_template('index.html', books=books, version=VERSION)
 
 
-# run the server 
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+  return render_template('create.html')
+
+
+# run the server
 port = int(os.environ['PORT'])
 app.run(host="0.0.0.0", port=port)
